f23c2c416ba4bcce7b0ac084bdb3b3d2c0faa061
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-01-14 13:34:31 -0800

    WIP: Trying to setup default checked checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,78 @@
 $(document).ready(function() {
 
   $('input').click(function () {
-    $('input:not(:checked)').parent().removeClass("checked");
-    $('input:checked').parent().addClass("checked");
+    $('input:not(:checked)').parent().removeClass('checked');
+    $('input:checked').parent().addClass('checked');
   });
-  $('input:checked').parent().addClass("checked");
+  $('input:checked').parent().addClass('checked');
 
   //Navigation buttons functionality
-  $("#start").click(function() {
-    $("#question1").show();
-    $(".lang_card").hide();
+  $('#start').click(function() {
+    $('.card').hide();
+
+    $('input').prop('checked', false);
+    $('input').each(function() {
+      if ($(this).val() === '1') {
+        $(this).prop('checked', true);
+        console.log("done");
+      } 
+
+    
+    });
+
+    $('.lang_card').hide();
+    $('#question1').show();
   });
 
-  $("#next1").click(function() {
-    $("#question1").hide();
-    $("#question2").show();
+  $('#next1').click(function() {
+    $('#question1').hide();
+    $('#question2').show();
   });
 
-  $("#next2").click(function() {
-    $("#question2").hide();
-    $("#question3").show();
+  $('#next2').click(function() {
+    $('#question2').hide();
+    $('#question3').show();
   });
 
-  $("#next3").click(function() {
-    $("#question3").hide();
-    $("#question4").show();
+  $('#next3').click(function() {
+    $('#question3').hide();
+    $('#question4').show();
   });
 
-  $("#next4").click(function() {
-    $("#question4").hide();
-    $("#question5").show();
+  $('#next4').click(function() {
+    $('#question4').hide();
+    $('#question5').show();
   });
 
-  $("#back2").click(function() {
-    $("#question2").hide();
-    $("#question1").show();
+  $('#back2').click(function() {
+    $('#question2').hide();
+    $('#question1').show();
   });
 
-  $("#back3").click(function() {
-    $("#question3").hide();
-    $("#question2").show();
+  $('#back3').click(function() {
+    $('#question3').hide();
+    $('#question2').show();
   });
 
-  $("#back4").click(function() {
-    $("#question4").hide();
-    $("#question3").show();
+  $('#back4').click(function() {
+    $('#question4').hide();
+    $('#question3').show();
   });
 
-  $("#back5").click(function() {
-    $("#question5").hide();
-    $("#question4").show();
+  $('#back5').click(function() {
+    $('#question5').hide();
+    $('#question4').show();
   });
 
   //Submit button functions
-  $("#suggester").submit(function(event) {
-    event.preventDefault();
+  $('#suggester').submit(function(event) {
+    event.preventDefault()
 
-    const qOne = $("input:radio[name=question1]:checked").val();
-    const qTwo = $("input:radio[name=question2]:checked").val();
-    const qThree = $("input:radio[name=question3]:checked").val();
-    const qFour = $("input:radio[name=question4]:checked").val();
-    const qFive = $("input:radio[name=question5]:checked").val();
+    const qOne = $('input:radio[name=question1]:checked').val();
+    const qTwo = $('input:radio[name=question2]:checked').val();
+    const qThree = $('input:radio[name=question3]:checked').val();
+    const qFour = $('input:radio[name=question4]:checked').val();
+    const qFive = $('input:radio[name=question5]:checked').val();
     let highestScore = 0;
 
     let ruby = 0;
@@ -68,43 +80,43 @@ $(document).ready(function() {
     let js = 0;
     let python = 0;
 
-    if (qOne === "1") {
+    if (qOne === '1') {
       ruby += 1
     } else {
       c += 1
     }
 
-    if (qTwo === "1") {
+    if (qTwo === '1') {
       js += 1
-    } else if (qTwo === "2") {
+    } else if (qTwo === '2') {
       python += 1
-    } else if (qTwo === "3") {
+    } else if (qTwo === '3') {
       ruby += 1
     } else {
       c += 1
     }
 
-    if (qThree === "1") {
+    if (qThree === '1') {
       js += 1
-    } else if (qThree === "2") {
+    } else if (qThree === '2') {
       python += 1
-    } else if (qThree === "3") {
+    } else if (qThree === '3') {
       ruby += 1
     } else {
       c += 1
     }
 
-    if (qFour === "1") {
+    if (qFour === '1') {
       js += 1
-    } else if (qFour === "2") {
+    } else if (qFour === '2') {
       python += 1
     } else {
       ruby += 1
     } 
 
-    if (qFive === "1") {
+    if (qFive === '1') {
       c += 1
-    } else if (qFive === "2") {
+    } else if (qFive === '2') {
       js += 1
     } else {
       python += 1
@@ -114,17 +126,17 @@ $(document).ready(function() {
     console.log(highestScore);
 
     if (ruby === highestScore) {
-      $("#ruby").show()
-      $("#question5").hide();
+      $('#ruby').show()
+      $('#question5').hide();
     } else if (c === highestScore) {
-      $("#c").show()
-      $("#question5").hide();
+      $('#c').show()
+      $('#question5').hide();
     } else if (js === highestScore) {
-      $("#js").show()
-      $("#question5").hide();
+      $('#js').show()
+      $('#question5').hide();
     } else {
-      $("#python").show()
-      $("#question5").hide();
+      $('#python').show()
+      $('#question5').hide();
     }
     
     
